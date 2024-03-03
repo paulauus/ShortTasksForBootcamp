c0ee1827413e4f628b032222bd744b47ba1d4f5b
Paula Uusnäkki <paula.uusnakki@gmail.com>
2024-03-03 17:30:06 +0000

    Change variable names

--- Task_01/details.py
+++ Task_01/details.py
@@ -4,9 +4,9 @@
 # ask for input on the user's street address
 # print out all the details entered above in one sentence
 
-age = input("How old are you? ")
-name = input("What is your name? ")
-house_number = input("What's your house number? ")
-street_address = input("What's your street address? ")
+user_age = input("How old are you? ")
+user_name = input("What is your name? ")
+user_house_number = input("What's your house number? ")
+user_street_address = input("What's your street address? ")
 
-print(name + " is " + age + " years old and lives at " + house_number + " " + street_address + ".")
+print(f"{user_name} is {user_age} years old and lives at {user_house_number} {user_street_address}.")
